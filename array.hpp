fb16f5a4d523e9b1daff951a3327acd872eaaaa2
animath <ath at mail dot animath dot xyz>
2025-01-10 00:16:12 +0700

    Added doc for Array and Persistent

--- array.hpp
+++ array.hpp
@@ -12,7 +12,9 @@ namespace pdc {
 
 using namespace internal;
 
-
+/*! Partially persistent array.
+ *
+ */
 template <typename T>
 class Array : public Persisent<Array<T>> {
   mutable std::shared_ptr<std::vector<FatNodes<T>>> array_;
@@ -20,20 +22,71 @@ class Array : public Persisent<Array<T>> {
   mutable std::shared_ptr<std::size_t> max_version_;
   mutable std::shared_ptr<FatNodes<std::size_t>> size_;
 public:
+  /*! \brief Default constructor. Create empty array. */
   Array();
+
+  /*! \brief Constructor with count of elements.
+   *
+   * \param count Count elements with default value.
+   */
   Array(std::size_t count);
+
+  /*! \brief Constructor with count of elements of a certain value.
+   *
+   * \param count Count of elements.
+   * \param value The value to use for initialization.
+   */
   Array(std::size_t count, T value);
   
+  /*! \brief Size of array. 
+   *
+   * \return Array size. 
+   */
   std::size_t Size() const { return GetSize(version_); }
+
+  /*! \brief Array empty? 
+   *
+   * \return true if еthe Array is empty, otherwise false.
+   */
   bool IsEmpty() const { return Size() == 0; }
+
+  /*! \brief Updates the value of the Array element.
+   *
+   * \param idx The index of the element to be changed. 
+   * \param value The new value of the element.
+   * \return New version of the Array with changed state.
+   * \throw std::out_of_range if idx is out of range.
+   */
   Array<T> Update(std::size_t idx, T value) const;
+
+  /*! \brief Add a value at the end of the array.
+   *
+   * \param value Value to add.
+   * \return New version of the array with changed state.
+   */
   Array<T> PushBack(T value) const;
 
+  /*! \brief Access the item for reading.
+   *
+   * \param idx The index of the element.
+   * \return Element to reading.
+   */
   const T& operator[](std::size_t idx) const 
     { return (*array_)[idx].Get(version_).value; }
 
+  /*! \brief Returns the previous version of the Array.
+   *
+   * Returns the same version of the Array if the version is minimal.
+   * \return Previous version of the Array.
+   */
   Array<T> Undo() const override
     { return Array<T>(*this, version_ > 0 ? version_ - 1 : version_); }
+
+  /*! \brief Returns the next version of the Array.
+   *
+   * Returns the same version of the Array if the version is maximum.
+   * \return Next version of the Array.
+   */
   Array<T> Redo() const override
     { return Array<T>(*this, version_ < *max_version_ ? version_ + 1 : version_); }
 
--- persistent_structure.hpp
+++ persistent_structure.hpp
@@ -2,10 +2,24 @@
 
 namespace pdc {
 
+/*! \brief Interface for persistent data structure.
+ * 
+ * \tparam Derived The type of the interface’s inherit structure. 
+ *                 Used as a return value type.
+*/
 template <typename Derived>
 class Persisent {
 public:
+  /*! \brief Returns the previous version of the structure.
+   *
+   * \return Previous version of the structure.
+   */
   virtual Derived Undo() const = 0;
+
+  /*! \brief Returns the next version of the structure.
+   *
+   * \return Next version of the structure.
+   */
   virtual Derived Redo() const = 0;
 };
 
